54742b74b75fb9d4396f23354883623ef891cc58
XAL-XAL <76600155+XAL-XAL@users.noreply.github.com>
2021-02-10 20:42:49 -0800

    Recent files added

--- src/com/company/Schedule.java
+++ src/com/company/Schedule.java
@@ -2,12 +2,31 @@ package com.company;
 
 public class Schedule {
 
-    int start_time;
-    int end_time;
+    public int start_time;
+    public int end_time;
 
     public static void main(String[] args) {
 
     }
 
-    public void
+    Schedule(int start_time, int end_time) {
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+
+    public void setStart_time(int start_time) {
+        this.start_time = start_time;
+    }
+
+    public int getStart_time(){
+        return this.start_time;
+    }
+
+    public void setEnd_time(int end_time){
+        this.end_time = end_time;
+    }
+
+    public int getEnd_time(){
+        return this.end_time;
+    }
 }
--- src/com/company/Vehicle.java
+++ src/com/company/Vehicle.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class Vehicle {
+
+    public string make;
+    public string model;
+    public int year;
+
+    public void setMake(string make){this.make=make;}
+    public void setModel(string model){this.model=model;}
+    public void setYear(int year){this.year=year;}
+
+    Vehicle(string make, string model, int year) {
+        this.make=make;
+        this.model=model;
+        this.year=year;
+    }
+
+    public string getMake() {
+        return make;
+    }
+    public
+
+    public string getModel() {
+        return model;
+    }
+    public int getYear(){
+        return year;
+    }
 }
